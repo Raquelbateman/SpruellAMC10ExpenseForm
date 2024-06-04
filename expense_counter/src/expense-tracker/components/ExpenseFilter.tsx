46bafa6958e9e9726671330b6e9e6d34cb0e1bb8
Raquelbateman <aliciaraquel1017@gmail.com>
2024-06-04 16:43:50 -0700

    update

--- expense_counter/src/expense-tracker/components/ExpenseFilter.tsx
+++ expense_counter/src/expense-tracker/components/ExpenseFilter.tsx
@@ -4,12 +4,12 @@ const ExpenseFilter = () => {
   return (
     <>
     <select className="form-select">
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
-  <option value="4">One</option>
-  <option value="5">Two</option>
-  <option value="6">Three</option>
+  <option value="">All Categories</option>
+  <option value="Groceries">Groceries</option>
+  <option value="Utilies">Utilies</option>
+  <option value="Entertainment">Entertainment</option>
+  <option value="Food">Food</option>
+  <option value="Shopping">Shopping</option>
 </select>
     
     </>
