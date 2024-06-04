dac04f9666ac6f4fbd6a8e4afeefe24066297d49
Raquelbateman <aliciaraquel1017@gmail.com>
2024-06-04 14:42:46 -0700

    update

--- expense_counter/src/App.tsx
+++ expense_counter/src/App.tsx
@@ -1,11 +1,11 @@
+import ExpenseList from "./expense-tracker/components/ExpenseList"
 
 
 const App = () => {
   return (
     <>
       <h1 className="text-center">Expense Tracker</h1>
-
-    
+    <ExpenseList/>
     </>
   )
 }
