9ab9acc97d0a431076dd6aa92f663ec69c3f1482
Raquelbateman <aliciaraquel1017@gmail.com>
2024-06-04 15:58:22 -0700

    update

--- expense_counter/src/App.tsx
+++ expense_counter/src/App.tsx
@@ -3,11 +3,7 @@ import ExpenseList from "./expense-tracker/components/ExpenseList"
 
 
 const App = () => {
-  const handleDelete = () =>{
 
-
-    
-  }
   const [dummyExpensesArray, setDummyExpensesArray] = useState([
     {id: 1, description: 'aaa', amount:10, category:'Utilities'},
     {id: 2, description: 'bbb', amount:15, category:'Entertainment'},
@@ -16,11 +12,17 @@ const App = () => {
     {id: 5, description: 'eee', amount:16, category:'Groceries'}
   ])
 
+  const handleDelete = (id:number) =>{
+    setDummyExpensesArray(dummyExpensesArray.filter(expense => expense.id !== id));
+
+    
+  }
+
 
   return (
     <>
       <h1 className="text-center">Expense Tracker</h1>
-    <ExpenseList expenses={dummyExpensesArray} onDelete={(id) => console.log ('Delete', id)}/>
+    <ExpenseList expenses={dummyExpensesArray} onDelete={handleDelete}/>
     </>
   )
 }
