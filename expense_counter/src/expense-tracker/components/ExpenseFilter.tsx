37930dbecc10474e5fee973d6ecfbe8b00179b2f
Raquelbateman <aliciaraquel1017@gmail.com>
2024-06-07 16:28:47 -0700

    updates

--- expense_counter/src/expense-tracker/components/ExpenseFilter.tsx
+++ expense_counter/src/expense-tracker/components/ExpenseFilter.tsx
@@ -12,7 +12,7 @@ const ExpenseFilter = ({onSelectCategory}:FilterProps) => {
     <select className="form-select" onChange={(e) => onSelectCategory(e.target.value)}>
   <option value="">All Categories</option>
   <option value="Groceries">Groceries</option>
-  <option value="Utilities">Utilies</option>
+  <option value="Utilities">Utilities</option>
   <option value="Entertainment">Entertainment</option>
   <option value="Food">Food</option>
   <option value="Shopping">Shopping</option>
