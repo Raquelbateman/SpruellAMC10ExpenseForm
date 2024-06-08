472e8dd309ae0d8acae35a18c76eb281be87b46b
Raquelbateman <aliciaraquel1017@gmail.com>
2024-06-07 17:02:00 -0700

    update

--- expense_counter/src/expense-tracker/components/ExpenseForm.tsx
+++ expense_counter/src/expense-tracker/components/ExpenseForm.tsx
@@ -3,8 +3,16 @@ const ExpenseForm = () => {
     <>
       <form action=""></form>
       <div className="mb-3">
-        <label htmlFor="" className="form-label"></label>
-        <input type="text" className="form-control" />
+        <label htmlFor="description" className="form-label">Description</label>
+        <input id="description" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">Amount</label>
+        <input id="amount" type="number" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">Category</label>
+        <select id="category" className="form-control"></select>
       </div>
     </>
   );
