0d45e6855c99cf93438c2a5be6c21e45ad041dc3
Raquelbateman <aliciaraquel1017@gmail.com>
2024-06-11 19:18:58 -0700

    update

--- expense_counter/src/expense-tracker/components/ExpenseForm.tsx
+++ expense_counter/src/expense-tracker/components/ExpenseForm.tsx
@@ -5,16 +5,16 @@ const ExpenseForm = () => {
     <>
       <form id="formEdit">
       <div className="mb-2">
-        <label htmlFor="descriptionInput" className="form-label" >Description</label>
-        <input id="descriptionInput" type="text" placeholder="" className="form-control" />
+        <label htmlFor="description" className="form-label" >Description</label>
+        <input id="description" type="text" className="form-control" />
       </div>
       <div className="mb-2">
-        <label htmlFor="amountInput" className="form-label">Amount</label>
-        <input id="amountInput" type="number" className="form-control" />
+        <label htmlFor="amount" className="form-label">Amount</label>
+        <input id="amount" type="number" className="form-control" />
       </div>
       <div className="mb-2">
-        <label htmlFor="categorySelect" className="form-label">Category</label>
-        <select id="categorySelect" className="form-control">
+        <label htmlFor="category" className="form-label">Category</label>
+        <select id="category" className="form-control">
         <option value=""></option>
         {categories.map(category => <option key={category}>{category}</option>)}
 
